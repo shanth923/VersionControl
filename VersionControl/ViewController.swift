1f2b98f7d4814ee6a0c028df10903531b59e3759
shanth kuamer <shanth923@gmail.com>
2020-01-10 14:30:27 +0530

    printed statement committed

--- VersionControl/ViewController.swift
+++ VersionControl/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+     
+        print("verodsdvdf ")
+        
+        
         // Do any additional setup after loading the view.
     }
 
